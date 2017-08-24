8828a499d0f1f0bcea307191d9eec20854cd0a74
GIN <millerbinbin@gmail.com>
2017-08-24 22:59:34 +0800

    refine layout

--- crawl/__init__.py
+++ crawl/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
+import os
 __author__ = 'hubin6'
 
 
-WORK_DIR = "C:\Users\hubin\PycharmProjects\dp"
+WORK_DIR = os.getcwd()
+print WORK_DIR
 SH_URL = "http://www.dianping.com/search/category/1/10"
 BAIDU_APP_KEY = "9MhIHvmWZHiQkEEoCIxKXGYkXbKS5hrq"
