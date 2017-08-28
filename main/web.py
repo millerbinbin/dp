5aac384cfc2a86fa94d329dc6152aaf41ff897a1
hubin6 <hubin6@jd.com>
2017-08-28 18:05:39 +0800

    correct name

--- main/web.py
+++ main/web.py
@@ -1,10 +1,17 @@
 #!flask/bin/python
 
 from flask_cors import *
+
+import os, sys
+#from crawl import WORK_DIR
+sys.path.append(WORK_DIR)
+
+print sys.path
 from app import app
 
-
 CORS(app, supports_credentials=True)
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0',port = 2222)
